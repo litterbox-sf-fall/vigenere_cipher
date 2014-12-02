efdbeb69e38d2f64a1df4d8298b3394035d2f5aa
iliastsangaris <iliastsangaris@gmail.com>
2014-12-01 20:02:12 -0800

    added comments to code & commented out sections that will be removed from exercise

--- src/vigenere.js
+++ src/vigenere.js
@@ -2,39 +2,40 @@ var vigenere = {};
 var helper = {};
 
 vigenere.encode = function(message, keyword) {
-  var alphabet = helper.alphabet();
-  var customKey = helper.keywordResize(message, keyword);
-  var splitMsg = message.split("");
+  // var alphabet = helper.alphabet();
+  // var customKey = helper.keywordResize(message, keyword);
+  // var splitMsg = message.split("");
 
-  var encodedLetters = splitMsg.map(function(element, index){
-    // finds the index of the encoded letter
-    var msgLetterPosition = alphabet.indexOf(element);
-    var keywordLetterPosition = alphabet.indexOf(customKey[index]);
-    var encodedLetterPosition = msgLetterPosition + keywordLetterPosition;
-    if(encodedLetterPosition > alphabet.length){ encodedLetterPosition -= alphabet.length; };
-    // returns the encoded letter
-    return alphabet[encodedLetterPosition];
-    // joins the array of encoded letters back together
-  })
+  // var encodedLetters = splitMsg.map(function(element, index){
+  //   // finds the index of each letter in the message
+  //   var msgLetterPosition = alphabet.indexOf(element);
+  //   // finds the index of each letter in the customKey
+  //   var keywordLetterPosition = alphabet.indexOf(customKey[index]);
+  //   // adds the index of message & customKey index together and 'circles' back if greater than alphabet
+  //   var encodedLetterPosition = msgLetterPosition + keywordLetterPosition;
+  //   if(encodedLetterPosition > alphabet.length){ encodedLetterPosition -= alphabet.length; };
+  //   // returns the new encoded letter
+  //   return alphabet[encodedLetterPosition];
+  //   // joins the array of encoded letters back together
+  // })
 
-  return encodedLetters.join("");
+  // return encodedLetters.join("");
 
 }
 
-vigenere.decode = function(message, keyword) {
-  var alphabet = helper.alphabet();
-  var customKey = helper.keywordResize(message, keyword);
-  var splitMsg = message.split("");
-
-  var decodedLetters = splitMsg.map(function(element, index){
-    var letterIndex = alphabet.indexOf(element) - alphabet.indexOf(customKey[index]);
-    if(letterIndex < 0){
-      letterIndex += alphabet.length;
-    };
-    return alphabet[letterIndex];
-  });
-  return decodedLetters.join("");
-}
+// vigenere.decode = function(message, keyword) {
+//   var alphabet = helper.alphabet();
+//   var customKey = helper.keywordResize(message, keyword);
+//   var splitMsg = message.split("");
+//   var decodedLetters = splitMsg.map(function(element, index){
+//     var letterIndex = alphabet.indexOf(element) - alphabet.indexOf(customKey[index]);
+//     if(letterIndex < 0){
+//       letterIndex += alphabet.length;
+//     };
+//     return alphabet[letterIndex];
+//   });
+//   return decodedLetters.join("");
+// }
 
 //Adjusts the side of the keyword to match the size of the message
 helper.keywordResize = function(message, keyword) {
